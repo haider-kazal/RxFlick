62aa6ccb3e914b562b2a4354d013c3097345397f
Haider Ali Kazal <haider_kazal@hotmail.com>
2020-04-20 01:18:02 +0600

    Initial configuration

--- RxFlick/ViewController.swift
+++ RxFlick/ViewController.swift
@@ -6,15 +6,33 @@
 //  Copyright © 2020 Haider Ali Kazal. All rights reserved.
 //
 
+import RxSwift
 import UIKit
 
 class ViewController: UIViewController {
-
+    let disposableBag: DisposeBag = .init()
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        Observable<Int>.timer(.seconds(0), period: .seconds(1), scheduler: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] (second) in
+                self?.label.text = "\(second)"
+            })
+            .disposed(by: disposableBag)
     }
-
-
 }
 
